9d3181ff998be4386fab47c436e59ea2edb6f7cb
Martina <mar03andres@gmail.com>
2026-06-16 23:13:26 -0300

    arreglé migration

--- prisma/migrations/20260616215656_make_ingrediente_obligatorio/migration.sql
+++ prisma/migrations/20260616215656_make_ingrediente_obligatorio/migration.sql
@@ -4,5 +4,8 @@
   - Made the column `ingrediente` on table `Producto` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- Fill nulls before making column required
+UPDATE "Producto" SET "ingrediente" = '' WHERE "ingrediente" IS NULL;
+
 -- AlterTable
 ALTER TABLE "Producto" ALTER COLUMN "ingrediente" SET NOT NULL;
